09c0846e3649ba022ff7e63ab430a542627aad05
carlstep <carlstep333@me.com>
2023-07-23 15:36:10 +0700

    new_item.dart function

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_cs1/data/dummy_items.dart';
+import 'package:shopping_list_cs1/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (ctx) => const NewItem()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries...'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: Icon(Icons.add),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
