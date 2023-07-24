4f0742095b743fff0a5d2b544f06e70943e1cdb9
carlstep <carlstep333@me.com>
2023-07-24 08:26:15 +0700

    removing items, no items features

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_cs1/data/dummy_items.dart';
+
 import 'package:shopping_list_cs1/models/grocery_item.dart';
 import 'package:shopping_list_cs1/widgets/new_item.dart';
 
@@ -27,8 +27,39 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items added!'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              height: 20,
+              width: 20,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries...'),
@@ -39,18 +70,7 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            height: 20,
-            width: 20,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(_groceryItems[index].quantity.toString()),
-        ),
-      ),
+      body: content,
     );
   }
 }
